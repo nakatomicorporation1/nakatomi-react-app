730aa4d6fce90a7df773fdcb925e5bdecf9663b4
Chang <shelbyhc@3c06300e19b6.ant.amazon.com>
2023-07-25 11:26:34 -0700

    added auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
-    <div className="App">
+    <View className="App">
       <header>
         <h1>Welcome to Nakatomi Corporation</h1>
       </header>
+      <Card>
+        {/* <Image src={logo} className="App-logo" alt="logo" /> */}
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
       <main>
         <h2>Upload CSV files here</h2>
         <form action="/action_page.php">
@@ -13,8 +27,8 @@ function App() {
           <input type="submit" />
         </form>
       </main>
-    </div>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
